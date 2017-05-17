c2de729d4f6712f2a21cf1ae09812caab062a700
Chase Hemerda <chasehemerda@icloud.com>
2017-05-17 12:07:47 -0600

    ALPHA V2

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+$('#myModal').modal('show');
+
+
 //MAP WITH ISS LOCATION
   var map = L.map('map').setView([0, 0], 3);
 
@@ -9,7 +12,6 @@ $(document).ready(function() {
       $.getJSON('https://galvanize-cors-proxy.herokuapp.com/http://api.open-notify.org/iss-now.json?callback=?', function(data) {
           var lat = data['iss_position']['latitude'];
           var lon = data['iss_position']['longitude'];
-
           iss.setLatLng([lat, lon]);
           isscirc.setLatLng([lat, lon]);
           map.panTo([lat, lon], animate=true);
@@ -24,11 +26,11 @@ $(document).ready(function() {
 
 //ISS MAP ICON
   var ISSIcon = L.icon({
-      iconUrl: 'images/station.png',
+      iconUrl: 'images/ISSIcon.png',
       iconSize: [50, 30],
       iconAnchor: [25, 15],
       popupAnchor: [50, 25],
-      shadowUrl: 'images/shadow.png',
+      shadowUrl: 'images/ISSIcon_shadow.png',
       shadowSize: [60, 40],
       shadowAnchor: [30, 15]
   });
@@ -38,33 +40,72 @@ $(document).ready(function() {
 
   moveISS();
 
+$("[name='dropdown']").change(function(){
+  $("[name='longitude']").attr("placeholder","")
+  $("[name='latitude']").attr("placeholder","")
+  var option = $("[name='dropdown']").val();
+    switch(option){
+      case "DALLAS":
+      $("[name='latitude']").val("32.7833333");
+      $("[name='longitude']").val("-96.8");
+      break;
+      case "DENVER":
+      $("[name='latitude']").val("39.7391667");
+      $("[name='longitude']").val("-104.9841667");
+      break;
+      case "HONOLULU":
+      $("[name='latitude']").val("21.3069444");
+      $("[name='longitude']").val("-157.8583333");
+      break;
+      case "JACKSONVILLE":
+      $("[name='latitude']").val("30.3319444");
+      $("[name='longitude']").val("-81.6558333");
+      break;
+      case "NEW YORK":
+      $("[name='latitude']").val("40.7141667");
+      $("[name='longitude']").val("-74.0063889");
+      break;
+      case "PHOENIX":
+      $("[name='latitude']").val("33.4483333");
+      $("[name='longitude']").val("-112.0733333");
+      break;
+      case "SAN FRANCISCO":
+      $("[name='latitude']").val("37.775");
+      $("[name='longitude']").val("-122.4183333");
+      break;
+      case "SEATTLE":
+      $("[name='latitude']").val("47.6063889");
+      $("[name='longitude']").val("-122.3308333");
+      break;
+    }
+})
+
+
 //PASS TIMES AND PEOPLE ON BOARD
 $(".btn").on("click", function(){
+  $(".modal-body").empty();
   var lats = $("[name='latitude']").val();
   var longs = $("[name='longitude']").val();
 //PASSING IN LAT AND LON VALUES FOR PASS TIMES
   $.getJSON("https://galvanize-cors-proxy.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=" + lats + "&lon=" + longs + "&alt=20&n=1&callback=?", function(data) {
       data['response'].forEach(function (d) {
           var date = new Date(d['risetime']*1000);
-           $('.passtimes').append('<h4>' + date.toString() + '</h4>');
+           $('.modal-body').append('<h3>WE DID OUR CALCULATIONS AND THE SPACE STATION WILL PASS YOU ON... </h3>' + "<h4>" + date.toString() + "</h4>");
       });
 
 //POPULATE NUM OF ASTRONAUTS AND NAMES
-  $.getJSON('http://api.open-notify.org/astros.json', function(data) {
+  $.getJSON('https://galvanize-cors-proxy.herokuapp.com/http://api.open-notify.org/astros.json', function(data) {
     var html = "";
-    var astroNum = "<h3>AND WAVE TO THE " + data.number + " ASTRONAUTS ON BOARD</h3>"
+    var astroNum = "<h3>MAKE SURE TO WAVE TO THE " + data.number + " ASTRONAUTS ON BOARD</h3>"
     for(people in data.people){
       html += "<h4>" + data.people[people].name.toUpperCase() + "</h4>"
     }
-      $(".onboard").append(html);
-      $(".astronautNum").append(astroNum)
+      $(".modal-body").append(astroNum)
+      $(".modal-body").append(html);
     });
   });
-  
-//CLEAR RESULTS
-  $('.passtimes').empty();
-  $('.astronautNum').empty();
-  $('.onboard').empty();
+
+
 })
 
 });
