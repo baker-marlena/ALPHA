4c866322bda64536eea9bcc3350337b3a5797aae
Chase Hemerda <chasehemerda@icloud.com>
2017-05-18 21:36:45 -0600

    ALPHA V3

--- script.js
+++ script.js
@@ -20,23 +20,23 @@ $("#myModal").modal("show");
   }
 
 //MAP TILE LAYERS
-  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hhc2Vyd2FzZXIyNSIsImEiOiJjajJxampiajAwM25jMndubjFqcDBydHRhIn0.KFNhZwQr6h5JY71n0mkfAQ", {
+  L.tileLayer("https://galvanize-cors-proxy.herokuapp.com/http://b.tiles.mapbox.com/v3/paldhous.earth_at_night,paldhous.cities_and_countries/{z}/{x}/{y}.png", {
       maxZoom: 5,
   }).addTo(map);
 
 //ISS MAP ICON
   var ISSIcon = L.icon({
-      iconUrl: "images/ISSIcon.png",
+      iconUrl: "images/iss.png",
       iconSize: [50, 30],
       iconAnchor: [25, 15],
       popupAnchor: [50, 25],
-      shadowUrl: "images/ISSIcon_shadow.png",
+      shadowUrl: "images/iss_shadow.png",
       shadowSize: [60, 40],
       shadowAnchor: [30, 15]
   });
 
   var iss = L.marker([0, 0], {icon: ISSIcon}).addTo(map);
-  var isscirc = L.circle([0,0], 2200e3, {color: "#c22", opacity: 0.3, weight:1, fillColor: "#c22", fillOpacity: 0.1}).addTo(map);
+  var isscirc = L.circle([0,0], 2200e3, {color: "#f5dda2", opacity: 0.3, weight:2, fillColor: "#000", fillOpacity: 0.1}).addTo(map);
 
   moveISS();
 
@@ -86,22 +86,23 @@ $(".btn").on("click", function(){
   $(".modal-body").empty();
   var lats = $("[name='latitude']").val();
   var longs = $("[name='longitude']").val();
+  var error = "<p>WE COULD NOT MAKE A PROPER CALCULATION. PLEASE CHECK YOUR LONGITUDE AND LATITUDE INPUTS!</p>"
 //PASSING IN LAT AND LON VALUES FOR PASS TIMES
 if(lats == "" || longs == ""){
-  $(".modal-body").text("WE COULD NOT MAKE A PROPER CALCULATION. PLEASE CHECK YOUR LONGITUDE AND LATITUDE INPUTS!")
+  $(".modal-body").html(error);
 
 } else {
 
   $.getJSON("https://galvanize-cors-proxy.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=" + lats + "&lon=" + longs + "&alt=20&n=1&callback=?", function(data) {
       data["response"].forEach(function (d) {
           var date = new Date(d['risetime']*1000);
-           $(".modal-body").append("<h4>WE DID OUR CALCULATIONS AND THE SPACE STATION WILL PASS YOU ON... </h4>" + "<h5>" + date.toString().toUpperCase() + "</h5>");
+           $(".modal-body").append("<h3>CALCULATIONS PREDICT THE SPACE STATION WILL PASS YOU ON... </h3>" + "<h5>" + date.toString().toUpperCase() + "</h5>");
       });
 
 //POPULATE NUM OF ASTRONAUTS AND NAMES
   $.getJSON('https://galvanize-cors-proxy.herokuapp.com/http://api.open-notify.org/astros.json', function(data) {
     var html = "";
-    var astroNum = "<h4>MAKE SURE TO WAVE TO THE " + data.number + " ASTRONAUTS ON BOARD</h4>"
+    var astroNum = "<h3>THERE ARE CURRENTLY " + data.number + " ASTRONAUTS ON BOARD</h3>"
     for(people in data.people){
       html += "<h5><a href='https://en.wikipedia.org/wiki/" + data.people[people].name + "'>" + data.people[people].name.toUpperCase() + "</a></h5>"
     }
